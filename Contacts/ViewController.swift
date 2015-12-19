e35b49e0e9350ad5695ce8ee40d4ab3a55ff69ad
jvadwala <jay.vadwala@mail.utoronto.ca>
2015-12-19 01:03:51 -0500

    Initial Commit

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Jay Vadwala on 2015-12-19.
+//  Copyright © 2015 Jay Vadwala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
